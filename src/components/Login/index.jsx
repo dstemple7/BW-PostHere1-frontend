ac73e2cbba62870ad8e6406badd278af79758dea
Nathan Galt <adiabatic@users.noreply.github.com>
2020-08-21 18:35:57 -0700

    Improve wording for TS upgrades in /Login/

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -4,7 +4,7 @@ import './style.scss'
 
 // If and when you're ready to upgrade this to TypeScript, simply change the extension of this file to .tsx.
 // Then, have me (Nathan) handy, because it'll probably immediately complain about implicit-any problems.
-// I'll walk you through them to get your code compiling again, and then we can work on getting better types for you, like typing the on-form-submit event as a React.FormEvent<HTMLFormElement>.
+// I'll walk you through them to get your code compiling again. After that, we can work on getting better types for you, like typing the on-form-submit event as a React.FormEvent<HTMLFormElement>.
 
 export default function Login() {
   return <></>
