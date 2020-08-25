0ceb0573746a111068b2b359aa2d223b7c30b4c5
Alex Williams <alexjwilliams1@gmail.com>
2020-08-25 09:32:45 -0700

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './components/Home'
 import Dashboard from './components/Dashboard'
-
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
--- src/components/CreatePost/index.tsx
+++ src/components/CreatePost/index.tsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import './style.scss'
 
+
+initialFormValues = {
+  title:'',
+  body:''
+}
+
 const CreatePost = (props: any) => {
+  useState[formValues, setFormValues] = useState(initialFormValues)
+
   return (
     <>
       <section className='create-post'>
         <h2>Create a post</h2>
         <div className='create-post-form'>
         <form >
-          <input placeholder='Title' type='text' />
+          <input placeholder='Title' 
+          type='text' 
+          value=''
+          />
           <input
             type='textarea'
             className='content-input'
             placeholder='Post content'
+            value=''
           />
           <div className='button-group'>
             <button>Get r/ Recommendation</button>
