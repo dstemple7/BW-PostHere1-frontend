9b389176bb8059defe9f776ba89de825883dd9be
Nathan Galt <adiabatic@users.noreply.github.com>
2020-08-25 16:07:26 -0700

    Prettier!

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -11,98 +11,94 @@ import './style.scss'
 const initialSignUpValues = {
   username: '',
   password1: '',
-  confirmPassword: ''
+  confirmPassword: '',
 }
 
 const initialErrorValues = {
   username: '',
   password1: '',
-  confirmPassword: ''
+  confirmPassword: '',
 }
 
 export default function SignUp() {
-
-  const [ signUpValues, setSignUpValues ] = useState(initialSignUpValues)
-  const [ errors, setErrors ] = useState(initialErrorValues);
-  const [ disabled, setDisabled ] = useState(true);
+  const [signUpValues, setSignUpValues] = useState(initialSignUpValues)
+  const [errors, setErrors] = useState(initialErrorValues)
+  const [disabled, setDisabled] = useState(true)
 
   useEffect(() => {
-    formSchema.isValid(signUpValues)
-      .then(valid => {
-        setDisabled(!valid);
-      })
+    formSchema.isValid(signUpValues).then((valid) => {
+      setDisabled(!valid)
+    })
   }, [signUpValues])
 
   const onChange = (evt) => {
     const name = evt.target.name
     const value = evt.target.value
-    validateChange( name, value)
+    validateChange(name, value)
   }
 
   const validateChange = (name, value) => {
-
     yup
       .reach(formSchema, name)
       .validate(value)
-      .then( valid => {
-        setErrors({...errors, [name]: ''})
+      .then((valid) => {
+        setErrors({ ...errors, [name]: '' })
       })
-      .catch( error => {
-        setErrors({...errors, [name]: error.errors[0]})
+      .catch((error) => {
+        setErrors({ ...errors, [name]: error.errors[0] })
       })
-    setSignUpValues({...signUpValues, [name]: value})
+    setSignUpValues({ ...signUpValues, [name]: value })
   }
-  
+
   const onSubmit = (evt) => {
     evt.preventDefault()
     const loginSubmit = {
       username: signUpValues.username.trim(),
-      password: signUpValues.password.trim()
+      password: signUpValues.password.trim(),
     }
     console.log(loginSubmit)
   }
 
   return (
-  <>
-    <div className='container'>
-      <form onSubmit={onSubmit}>
-        <h1>Sign Up</h1>
-        <label>Username:</label>
-        <p className='error'>{errors.username}</p>
-        <input
-          name='username'
-          type='text'
-          placeholder='username'
-          value={signUpValues.username}
-          onChange={onChange}
-        />
-        <label>Password:</label>
-        <p className='error'>{errors.password}</p>
-        <input
-          name='password'
-          type='password'
-          placeholder='password'
-          value={signUpValues.password}
-          onChange={onChange}
-        />
-         <label>Confirm Password:</label>
-        <p className='error'>{ !disabled ? null : errors.confirmPassword}</p>
-        <input
-          name='confirmPassword'
-          type='password'
-          placeholder='password'
-          value={signUpValues.confirmPassword}
-          onChange={onChange}
-        />
-        <input
-          name='submit'
-          type='submit'
-          value='sign up'
-          disabled={disabled}
-
-        />
-      </form>
-    </div>
-  </>
+    <>
+      <div className='container'>
+        <form onSubmit={onSubmit}>
+          <h1>Sign Up</h1>
+          <label>Username:</label>
+          <p className='error'>{errors.username}</p>
+          <input
+            name='username'
+            type='text'
+            placeholder='username'
+            value={signUpValues.username}
+            onChange={onChange}
+          />
+          <label>Password:</label>
+          <p className='error'>{errors.password}</p>
+          <input
+            name='password'
+            type='password'
+            placeholder='password'
+            value={signUpValues.password}
+            onChange={onChange}
+          />
+          <label>Confirm Password:</label>
+          <p className='error'>{!disabled ? null : errors.confirmPassword}</p>
+          <input
+            name='confirmPassword'
+            type='password'
+            placeholder='password'
+            value={signUpValues.confirmPassword}
+            onChange={onChange}
+          />
+          <input
+            name='submit'
+            type='submit'
+            value='sign up'
+            disabled={disabled}
+          />
+        </form>
+      </div>
+    </>
   )
 }
