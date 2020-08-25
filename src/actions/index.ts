b5f840f79ad057860cf593d8fd620bace6e36117
Nathan Galt <adiabatic@users.noreply.github.com>
2020-08-25 13:00:53 -0700

    Get recs and incorporate them into ApplicationState

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,9 @@
 import { SignupAction } from './signup'
 import { LoginErrorAction } from './login'
+import { GetRecommendationsAction } from './recs'
 
 export * from './signup'
 export * from './login'
 export * from './recs'
 
-export type Action = SignupAction | LoginErrorAction
+export type Action = SignupAction | GetRecommendationsAction | LoginErrorAction
