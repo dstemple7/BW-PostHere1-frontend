3395b92856bac64ab86e288602a18eee2607830c
git-austinmccollom <austinmccollom@gmail.com>
2020-08-26 19:48:14 -0500

    Made all the trending subreddits clickable links. Also added title and some styling

--- src/components/Trending/index.js
+++ src/components/Trending/index.js
@@ -4,11 +4,14 @@ import axios from 'axios';
 
 export default function Trending(props) {
   const [ subreddits, setSubreddits ] = useState([])
+  const [ link, setLink ] = useState()
 
   useEffect( () => {
     axios.get('https://www.reddit.com/api/trending_subreddits.json')
       .then( res => {
+        console.log(res.data)
         setSubreddits(res.data.subreddit_names)
+        setLink(res.data.comment_url)
       })
       .catch( err => {
         console.log(err)
@@ -17,12 +20,13 @@ export default function Trending(props) {
 
   return(
     <div className='trending-post'>
-
+      <h1>Today's trending subreddits:</h1>
       { subreddits.map( sub => {
-        return <h4>{`r/${sub}`}</h4>
+        return (
+        <a href={`https://www.reddit.com/r/${sub}`}><h4>{`r/${sub}`}</h4></a>
+        )
       })
       }
-      {}
     </div>
   )
 }
