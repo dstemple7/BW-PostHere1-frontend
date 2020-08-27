dedb5fb10a9055b96c14cdcc057bf073df35b801
Nathan Galt <adiabatic@users.noreply.github.com>
2020-08-27 12:04:48 -0700

    Fix subreddit suggestions

--- src/components/EditSavedPost/index.js
+++ src/components/EditSavedPost/index.js
@@ -7,8 +7,25 @@ import './style.scss'
 import intersperse from '../../util/intersperse'
 
 const EditSavedPost = (props) => {
-
   const { post } = props
+
+  try {
+    post.subredditsAsList = JSON.parse(post.subreddit)
+  } catch (e) {
+    console.error(
+      'Error trying to parse post.subreddit, which is a string containing JSON, maybe. The post was',
+      post
+    )
+    throw e
+  }
+
+  const [subredditSuggestions, setSubredditSuggestions] = useState(
+    post.subredditsAsList
+  )
+
+  if (subredditSuggestions === undefined) {
+    console.log('the entire post, with an undefined post.subreddits', post)
+  }
 
   const [title, setTitle] = useState(post.title)
   const [body, setBody] = useState(post.post)
@@ -38,6 +55,15 @@ const EditSavedPost = (props) => {
     props.setIsEditing(false)
   }
 
+  console.log('This is the subreddit suggestions:', subredditSuggestions)
+
+  const suggestedElements = subredditSuggestions.map((s) => (
+    <a
+      data-probability={s.probability}
+      href={`https://www.reddit.com/r/${s.subreddit}`}
+    >{`/r/${s.subreddit}`}</a>
+  ))
+
   return (
     <div>
       <form className='edit-saved-post' onSubmit={onSubmit}>
@@ -66,7 +92,11 @@ const EditSavedPost = (props) => {
         </div>
         <div className='suggestions'>
           <p>Subreddit Suggestions:</p>
-          {/* <p>{subRedditSuggestions === "" ? "" : intersperse(subRedditSuggestions, ' · ')}</p> */}
+          <p>
+            {subredditSuggestions === ''
+              ? ''
+              : intersperse(suggestedElements, ' · ')}
+          </p>
         </div>
         <div className='button-group'>
           <button onClick={handlePostUpdate}>Done</button>
@@ -81,5 +111,7 @@ const EditSavedPost = (props) => {
 
 const mapStateToProps = (state) => state
 
-
-export default connect(mapStateToProps, { getRecommendations, updateSavedPost })(EditSavedPost)
+export default connect(mapStateToProps, {
+  getRecommendations,
+  updateSavedPost,
+})(EditSavedPost)
